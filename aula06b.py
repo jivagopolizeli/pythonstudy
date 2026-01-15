0b6f9568ca8b93667fe68450efad8f001f21792e
jivagopolizeli <jivagopolizeli@gmail.com>
2026-01-15 15:14:44 -0300

    Add files via upload

---
+++ aula06b.py
@@ -0,0 +1,8 @@
+n=input('Digite algo: ')
+print(f'Só tem espaços? {n.isspace()}')
+print(f'É numérico? {n.isnumeric()}')
+print(f'É alfabético? {n.isalpha()}')
+print(f'É alfanumérico? {n.isalnum()}')
+print(f'Está em letras maiúsculas? {n.isupper()}')
+print(f'Está em letras minúsculas? {n.islower()}')
+print(f'Está capitalizada? {n.istitle()}')
